22c3b2938504943a839aeed81fdd6f17feea6132
Greenzin1 <Greenzin1@users.noreply.github.com>
2026-09-12 11:25:35 +0000

    feat: add debug logging + FORCE_FILESYSTEM [skip ci]

--- platforms/Emscripten.cmake
+++ platforms/Emscripten.cmake
@@ -1,7 +1,7 @@
 add_executable(RetroEngine ${RETRO_FILES})
 
 target_compile_options(RetroEngine PRIVATE -sUSE_SDL=2 -sUSE_OGG=1 -sUSE_VORBIS=1)
-target_link_options(RetroEngine PRIVATE -sUSE_SDL=2 -sUSE_OGG=1 -sUSE_VORBIS=1 -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=33554432)
+target_link_options(RetroEngine PRIVATE -sUSE_SDL=2 -sUSE_OGG=1 -sUSE_VORBIS=1 -sFORCE_FILESYSTEM=1 -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=33554432)
 
 target_link_options(RetroEngine PRIVATE
     "SHELL:-sEXPORTED_FUNCTIONS=[\"_main\"]"

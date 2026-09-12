03ec7b34dc2a14912b3704cb2d7594617c59659d
Greenzin1 <Greenzin1@users.noreply.github.com>
2026-09-12 00:45:02 +0000

    fix: escape exported functions properly in cmake [skip ci]

--- platforms/Emscripten.cmake
+++ platforms/Emscripten.cmake
@@ -3,7 +3,10 @@ add_executable(RetroEngine ${RETRO_FILES})
 target_compile_options(RetroEngine PRIVATE -sUSE_SDL=2 -sUSE_OGG=1 -sUSE_VORBIS=1)
 target_link_options(RetroEngine PRIVATE -sUSE_SDL=2 -sUSE_OGG=1 -sUSE_VORBIS=1 -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=33554432)
 
-target_link_options(RetroEngine PRIVATE -sEXPORTED_FUNCTIONS='["_main"]' -sEXPORTED_RUNTIME_METHODS='["ccall","cwrap"]')
+target_link_options(RetroEngine PRIVATE
+    "SHELL:-sEXPORTED_FUNCTIONS=[\"_main\"]"
+    "SHELL:-sEXPORTED_RUNTIME_METHODS=[\"ccall\",\"cwrap\"]"
+)
 
 if(RETRO_USE_MOD_LOADER)
     set_target_properties(RetroEngine PROPERTIES

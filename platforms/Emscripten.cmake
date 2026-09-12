4c16f4e04093d404ac519def9a8d1921b862d0e1
Greenzin1 <Greenzin1@users.noreply.github.com>
2026-09-12 00:37:11 +0000

    fix: use only linker flags for emscripten ports [skip ci]

--- platforms/Emscripten.cmake
+++ platforms/Emscripten.cmake
@@ -1,25 +1,10 @@
 add_executable(RetroEngine ${RETRO_FILES})
 
-target_compile_options(RetroEngine PRIVATE -s USE_SDL=2)
-target_link_options(RetroEngine PRIVATE -s USE_SDL=2 -s ALLOW_MEMORY_GROWTH=1 -s INITIAL_MEMORY=33554432 --closure 0)
+set(EMSCRIPTEN_FLAGS "-sUSE_SDL=2 -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=33554432 -sUSE_OGG=1 -sUSE_VORBIS=1 --closure 0")
 
-if(COMPILE_OGG)
-    target_compile_options(RetroEngine PRIVATE -s USE_OGG=1)
-    target_link_options(RetroEngine PRIVATE -s USE_OGG=1)
-else()
-    target_compile_options(RetroEngine PRIVATE -s USE_OGG=1)
-    target_link_options(RetroEngine PRIVATE -s USE_OGG=1)
-endif()
+target_link_options(RetroEngine PRIVATE ${EMSCRIPTEN_FLAGS})
 
-if(COMPILE_VORBIS)
-    target_compile_options(RetroEngine PRIVATE -s USE_VORBIS=1)
-    target_link_options(RetroEngine PRIVATE -s USE_VORBIS=1)
-else()
-    target_compile_options(RetroEngine PRIVATE -s USE_VORBIS=1)
-    target_link_options(RetroEngine PRIVATE -s USE_VORBIS=1)
-endif()
-
-target_link_options(RetroEngine PRIVATE -s EXPORTED_FUNCTIONS='["_main"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]')
+target_link_options(RetroEngine PRIVATE -sEXPORTED_FUNCTIONS='["_main"]' -sEXPORTED_RUNTIME_METHODS='["ccall","cwrap"]')
 
 if(RETRO_USE_MOD_LOADER)
     set_target_properties(RetroEngine PROPERTIES
